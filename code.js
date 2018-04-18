978c89de53cee718b02260bc4817e1baf4aad14d
Andrew Clemens <apc@press.jhu.edu>
2018-04-18 10:48:11 -0400

    external links

--- code.js
+++ code.js
@@ -17,6 +17,7 @@ $('#location-form').on('submit', function(e) {
     var b = moment(endDate, 'DD-MM-YYYY');
     var numDays = b.diff(a, 'days');
 
+    /*
     $.ajax({
         url: theatersUrl,
         data: {
@@ -30,15 +31,14 @@ $('#location-form').on('submit', function(e) {
         },
         dataType: "jsonp",
     });
+    */
 
-    /*
     $.ajax({
         dataType: "json",
         url: "data.json",
         data: {},
         success: dataHandler,
     })
-    */
 });
 
 function dataHandler(data) {
@@ -46,6 +46,7 @@ function dataHandler(data) {
     vm.movies().forEach(function(m){
         m.theaters = [];
         if(!m.hasOwnProperty('directors')) m.directors = ['N/A'];
+        if(!m.hasOwnProperty('releaseYear')) m.releaseYear = '';
         if(!m.hasOwnProperty('topCast')) m.topCast = ['N/A'];
         if(!m.hasOwnProperty('longDescription')) m.longDescription = 'N/A';
         if(!m.hasOwnProperty('genres')) m.genres = ['N/A'];
